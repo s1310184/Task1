cf1d6c00624d414059b6ca19e2719dc2dc93eeee
yoshitomi5103 <136326336+yoshitomi5103@users.noreply.github.com>
2024-07-04 15:05:47 +0900

    Victory message added

--- Task1.py
+++ Task1.py
@@ -15,3 +15,8 @@ for round in range(1, 4):
         tails_count += 1
 
 print(f"Heads: {heads_count}, Tails: {tails_count}")
+
+if heads_count > tails_count:
+    print("You won")
+else:
+    print("You lost")
